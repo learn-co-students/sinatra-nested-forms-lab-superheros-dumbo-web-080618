0b96ce6a9d0c599cbd1611366acc43fb6b89432e
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2018-08-28 11:35:52 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+
+    get "/" do
+      erb :super_hero
+    end
+
+    post "/team" do
+      @team = Team.new(params[:team])
+      params[:team][:members].each do |details|
+      Hero.new(details)
+    end
+    @hero = Hero.all
+      #binding.pry
+
+      erb :team
+    end
 
 
 end
